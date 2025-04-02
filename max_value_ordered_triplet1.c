25c3798c3d27de42fa7019951f86756f9b65b238
Jack Sangdahl <jack@pngu.org>
2025-04-01 21:04:34 -0600

    2873. maximum value of an ordered triplet 1

---
+++ max_value_ordered_triplet1.c
@@ -0,0 +1,28 @@
+// 2873. Maximum Value of an Ordered Triplet I
+#include "leetcode.h"
+
+/*
+ * you are given a 0-indexed integer array 'nums'. return the maximum value over
+ * all triplets of indices '(i, j, k)' such that 'i < j < k'. if all such
+ * triplets have a negative value, return 0. the value of a triplet of indices
+ * '(i, j, k)' is equal to '(nums[i] - nums[j]) * nums[k]'
+ */
+
+long long maximumTripletValue(int *nums, int numsSize) {
+  if (numsSize < 3)
+    return 0;
+  long long ans = 0, curr = nums[0], diff = 0;
+  for (int i = 1; i < numsSize; i++) {
+    ans = fmax(ans, (long long)diff * nums[i]);
+    diff = fmax(diff, curr - nums[i]);
+    curr = fmax(curr, nums[i]);
+  }
+  return ans;
+}
+
+int main() {
+  int n1[] = {12, 6, 1, 2, 7}, n2[] = {1, 10, 3, 4, 19}, n3[] = {1, 2, 3};
+  printf("%lld\n", maximumTripletValue(n1, ARRAY_SIZE(n1))); // expect: 77
+  printf("%lld\n", maximumTripletValue(n2, ARRAY_SIZE(n2))); // expect: 133
+  printf("%lld\n", maximumTripletValue(n3, ARRAY_SIZE(n3))); // expect: 0
+}
---
+++ max_value_ordered_triplet1.py
@@ -0,0 +1,32 @@
+# 2873. Maximum Value of an Ordered Triplet I
+
+"""
+you are given a 0-indexed integer array 'nums'. return the maximum value over
+all triplets of indices '(i, j, k)' such that 'i < j < k'. if all such
+triplets have a negative value, return 0. the value of a triplet of indices
+'(i, j, k)' is equal to '(nums[i] - nums[j])  nums[k]'
+"""
+
+
+class Solution(object):
+    def maximumTripletValue(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n < 3:
+            return 0
+        ans, curr, diff = 0, nums[0], 0
+        for i in range(1, n):
+            ans = max(ans, diff * nums[i])
+            diff = max(diff, curr - nums[i])
+            curr = max(curr, nums[i])
+        return ans
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.maximumTripletValue(nums=[12, 6, 1, 2, 7]))
+    print(obj.maximumTripletValue(nums=[1, 10, 3, 4, 19]))
+    print(obj.maximumTripletValue(nums=[1, 2, 3]))
