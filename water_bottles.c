9885ede0c5c6394f0196b2e7d459105d77749c07
Jack Sangdahl <jack@pngu.org>
2025-09-30 21:49:16 -0600

    1518. water bottles

--- water_bottles.c
+++ water_bottles.c
@@ -10,14 +10,7 @@
  */
 
 int numWaterBottles(int numBottles, int numExchange) {
-  int ans = numBottles;
-  while (numBottles >= numExchange) {
-    int rem = numBottles % numExchange;
-    numBottles /= numExchange;
-    ans += numBottles;
-    numBottles += rem;
-  }
-  return ans;
+  return numBottles + (numBottles - 1) / (numExchange - 1);
 }
 
 int main() {
