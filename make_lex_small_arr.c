ea42a655c88448187c3e82ef93540f89b490e97d
Jack Sangdahl <jack@pngu.org>
2026-08-29 17:19:04 -0600

    2948. make lexicogrphically smallest array by swapping elements

--- make_lex_small_arr.c
+++ make_lex_small_arr.c
@@ -9,54 +9,66 @@
  * operation any number of times.
  */
 
-int cmp1(const void *a, const void *b) {
-  int *aa = (int *)a;
-  int *bb = (int *)b;
-  return aa[0] - bb[0];
-}
+struct pair {
+  int val;
+  int idx;
+};
 
-int cmp2(const void *a, const void *b) { return *(int *)a - *(int *)b; }
+int cmp(const void *a, const void *b) {
+  return ((struct pair *)a)->val - ((struct pair *)b)->val;
+}
 
 int *lexicographicallySmallestArray(int *nums, int numsSize, int limit,
                                     int *returnSize) {
-  int *ans = (int *)malloc(numsSize * sizeof(int));
-  *returnSize = numsSize;
-  int cpy[numsSize][2];
+  struct pair *p = (struct pair *)malloc(numsSize * sizeof(struct pair));
   for (int i = 0; i < numsSize; i++) {
-    cpy[i][0] = nums[i];
-    cpy[i][1] = i;
+    p[i].val = nums[i];
+    p[i].idx = i;
   }
-  qsort(cpy, numsSize, sizeof(cpy[0]), cmp1);
-  int indices[numsSize];
-  for (int i = 0; i < numsSize;) {
-    int size = 0, prev = cpy[i][0];
-    while (i < numsSize && cpy[i][0] <= (prev + limit)) {
-      indices[size++] = cpy[i][1];
-      prev = cpy[i][0];
-      i++;
+  qsort(p, numsSize, sizeof(struct pair), cmp);
+  int *ans = (int *)malloc(numsSize * sizeof(int));
+  int *pos = (int *)malloc(numsSize * sizeof(int));
+  int prev = INT_MIN, pos_cnt = 0;
+  for (int s = 0, e = 0; e < numsSize;) {
+    pos[pos_cnt++] = p[e].idx;
+    prev = p[e].val;
+    e++;
+    if (e == numsSize || p[e].val - prev > limit) {
+      qsort(pos, pos_cnt, sizeof(int), cmp);
+      for (int i = 0; i < pos_cnt; i++) {
+        ans[pos[i]] = p[s].val;
+        s++;
+      }
+      pos_cnt = 0;
     }
-    qsort(indices, size, sizeof(int), cmp2);
-    for (int j = 0, k = i - size; j < size; j++, k++)
-      ans[indices[j]] = cpy[k][0];
   }
+  free(p);
+  free(pos);
+  *returnSize = numsSize;
   return ans;
 }
 
 int main() {
-  int n1[] = {1, 5, 3, 9, 8}, rs1;
-  int n2[] = {1, 7, 6, 18, 2, 1}, rs2;
-  int n3[] = {1, 7, 28, 19, 10}, rs3;
+  int n1[] = {1, 5, 3, 9, 8}, r1[] = {1, 3, 5, 8, 9}, rs1;
+  int n2[] = {1, 7, 6, 18, 2, 1}, r2[] = {1, 6, 7, 18, 1, 2}, rs2;
+  int n3[] = {1, 7, 28, 19, 10}, r3[] = {1, 7, 28, 19, 10}, rs3;
   int *lsa1 = lexicographicallySmallestArray(n1, ARRAY_SIZE(n1), 2, &rs1);
   int *lsa2 = lexicographicallySmallestArray(n2, ARRAY_SIZE(n2), 3, &rs2);
   int *lsa3 = lexicographicallySmallestArray(n3, ARRAY_SIZE(n3), 3, &rs3);
-  for (int i = 0; i < rs1; i++)
-    printf("%d ", lsa1[i]); // expect: 1 3 5 8 9
+  for (int i = 0; i < rs1; i++) {
+    printf("%d ", lsa1[i]);
+    assert(lsa1[i] == r1[i]);
+  }
   printf("\n");
-  for (int i = 0; i < rs2; i++)
-    printf("%d ", lsa2[i]); // expect: 1 6 7 18 1 2
+  for (int i = 0; i < rs2; i++) {
+    printf("%d ", lsa2[i]);
+    assert(lsa2[i] == r2[i]);
+  }
   printf("\n");
-  for (int i = 0; i < rs3; i++)
-    printf("%d ", lsa3[i]); // expect: 1 7 28 19 10
+  for (int i = 0; i < rs3; i++) {
+    printf("%d ", lsa3[i]);
+    assert(lsa3[i] == r3[i]);
+  }
   printf("\n");
   free(lsa1);
   free(lsa2);
--- make_lex_small_arr.py
+++ make_lex_small_arr.py
@@ -9,24 +9,24 @@ operation any number of times.
 """
 
 
-class Solution(object):
+class Solution:
     def lexicographicallySmallestArray(self, nums, limit):
         """
         :type nums: List[int]
         :type limit: int
         :rtype: List[int]
         """
-        n, indices = len(nums), []
-        cpy, ans = sorted([(nums[i], i) for i in range(n)]), [0] * n
-        for i in range(n):
-            if i == 0 or cpy[i][0] - cpy[i - 1][0] > limit:
-                indices.append([])
-            indices[-1].append(cpy[i][1])
-        for i in indices:
-            curr = sorted(i)
-            for j in range(len(i)):
-                ans[curr[j]] = nums[i[j]]
-        return ans
+        arr = sorted((v, i) for i, v in enumerate(nums))
+        i = 0
+        while i < len(nums):
+            j = i + 1
+            while j < len(nums) and arr[j][0] - arr[j - 1][0] <= limit:
+                j += 1
+            idx = sorted(x[1] for x in arr[i:j])
+            for k in range(len(idx)):
+                nums[idx[k]] = arr[i + k][0]
+            i = j
+        return nums
 
 
 if __name__ == "__main__":
